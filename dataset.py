6f0c92bf4dcac4fc0a57ba93eb7db9b6780ae1b4
hewlett-packard-lovecraft <hx2314@nyu.edu>
2026-04-03 00:36:31 -0400

    quantization quiz

--- dataset.py
+++
@@ -1,1 +0,0 @@
-# script to prepare a dataset
--- scripts/dataset.py
+++ scripts/dataset.py
@@ -1,19 +1,34 @@
 # split sound files into 3-second chunks and save them as .npy files for quantization
 
-from logging import _srcfile
+import argparse
 from pathlib import Path
-from sqlite3 import register_adapter
 import librosa
 import numpy as np
-from numpy.typing import NDArray
+import numpy.typing as npt
 from datetime import datetime
 
+DOCKER_DATASET_ABSOLUTE_PATH = "/usr/src/birdnet/dataset/"
 
-def split_chunks(sig, rate=48000, overlap=0.0, seconds=3.0, min_len=1.5):
-    chunks = []
 
-    for i in range(0, len(sig), int(seconds - overlap) * rate):
-        chunk = sig[i : i + int(seconds * rate)]
+def split_audio_file(
+    src: Path,
+    dest: Path,
+    rate: float = 48000,
+    overlap: float = 0.0,
+    seconds: float = 3.0,
+    min_len: float = 1.5,
+):
+    print(f"Reading {src.name} ... ", end=" ", flush=True)
+
+    # see: https://github.com/birdnet-team/BirdNET-Lite/blob/main/analyze.py#L115
+    signal, rate = librosa.load(
+        path=src.absolute(), sr=rate, mono=True, res_type="kaiser_fast"
+    )
+
+    chunks: list[npt.NDArray[np.float64]] = []
+
+    for i in range(0, len(signal), int((seconds - overlap) * rate)):
+        chunk = signal[i : i + int(seconds * rate)]
 
         if len(chunk) < int(min_len * rate):
             break
@@ -21,46 +36,76 @@ def split_chunks(sig, rate=48000, overlap=0.0, seconds=3.0, min_len=1.5):
         if len(chunk) < int(rate * seconds):
             temp = np.zeros((int(rate * seconds)))
             temp[: len(chunk)] = chunk
-            split = temp
+            chunk = temp
 
         chunks.append(chunk)
 
-    return chunks
-
-
-def load_audio_file(src: Path, rate=48000, overlap=0.0):
-    print(f"Reading {src.name} ... ", end=" ", flush=True)
-    signal, rate = librosa.load(
-        path=str(src.absolute()), sr=48000, mono=True, res_type="kaiser_fast"
-    )
-
-    chunks = split_chunks(signal, rate, overlap)
-
     print(f"Done! Read {str(len(chunks))} chunks. ")
 
-    return chunks
-
-
-def write_chunks(src: Path, dest: Path, chunks: NDArray[np.float64]):
     file_name = src.name
-    timestamp = datetime.now()
+    # timestamp = datetime.now()
 
     print(f"Writing {file_name} chunks to {dest.name}")
 
+    chunk_paths: list[str] = []
+
     for i, chunk in enumerate(chunks):
         chunk_name: str = (
-            timestamp.strftime("%Y-%m-%d-%H:%M:%S") + file_name + "_" + str(i) + ".npy"
+            # timestamp.strftime("%Y-%m-%d-%H:%M:%S") +
+            file_name + "_" + str(i) + ".npy"
         )
         chunk_dest = dest / chunk_name
 
         np.save(str(chunk_dest.absolute()), chunk)
+        chunk_paths.append(DOCKER_DATASET_ABSOLUTE_PATH + "/" + chunk_name + "\n")
+
+    return chunk_paths
+
+
+class Args(argparse.Namespace):
+    src: list[str] = ["../example/"]
+    dest: str = "../dataset/"
+    txt: str = "../dataset.txt"
 
 
 def main():
-    samples = Path("../example/")
+    parser = argparse.ArgumentParser()
+    _ = parser.add_argument(
+        "--src",
+        type=list[str],
+        # action="append",
+        nargs="+",
+        help="Path to input file directory",
+        default=["../example/"],
+    )
 
-    for s in samples.iterdir():
-        s.absolute
+    _ = parser.add_argument("--dest", type=str, help="Path to output file directory")
+    _ = parser.add_argument(
+        "--txt", type=str, help="Path to dataset.txt", default="../dataset.txt"
+    )
+
+    args = parser.parse_args(namespace=Args())
+
+    src: list[str] = args.src
+    dest = Path(args.dest)
+    txt = Path(args.txt)
+
+    dataset_txt: list[str] = []
+
+    for dir in src:
+        dir = "".join(dir)
+        print(f"Reading {dir} ...")
+
+        for s in Path(dir).iterdir():
+            if s.suffix != ".mp3" and s.suffix != ".wav":
+                continue
+
+            dataset_txt += split_audio_file(src=s, dest=dest)
+
+    with open(txt, "w") as f:
+        f.writelines(dataset_txt)
+
+    print(f"Done! wrote {str(len(dataset_txt))} lines to {txt}. ")
 
 
 if __name__ == "__main__":

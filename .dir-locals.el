e2a4cdfc912b458af31021a3d67d07589c3b2246
Hao Ming Xia <hx2314@nyu.edu>
2026-04-09 15:24:24 -0400

    cleanup & update readme

---
+++ .dir-locals.el
@@ -0,0 +1,13 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+
+
+
+;; (add-to-list 'tramp-remote-path "/home/hxia/git/BirdNET-Model-V2.4-RKNN/.venv/bin")
+
+;; (with-eval-after-load 'eglot
+;;   (add-to-list 'eglot-server-programs
+;;                '(python-mode . (".venv/bin/basedpyright-langserver" "--stdio"))))
+
+
